a7ae0c0a9267030f0d2a4a0c6656d87e5ab2e8f2
ChouYiTing <acs113102@gm.ntcu.edu.tw>
2026-05-27 21:28:46 +0800

    feat: xampp

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>家教媒合平台</title>
+        <link rel="icon" type="image/png" href="favicon.png">
+    </head>
+    <body>
+        <h1>123</h1>
+        <?php 
+            $begin=12;
+            $end=18;
+            for($i=$begin;$i<=$end;$i+=3){ 
+        ?>
+        <p><?php echo $i ?></p>
+        <?php } ?>
+    </body>
+</html>
